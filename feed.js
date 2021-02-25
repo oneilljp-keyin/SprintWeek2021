c58b66a1ab4bd627cbc9cb668b7f8eb5daf5da1c
oneilljp-keyin <john.oneill@keyin.com>
2021-02-25 08:06:20 -0330

    Finish pages, can't get all css in same file

--- feed.js
+++ feed.js
@@ -10,10 +10,21 @@
 
 // This function will retrieve info from local storage to get name, join date, friends and followers.
 
+function getTimeStamp() {
+    let today = new Date(Date.now());
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    let hh = String(today.getHours()).padStart(2, '0');
+    let min = String(today.getMinutes()).padStart(2, '0');
+
+    return yyyy+"-"+mm+"-"+dd+" "+hh+":"+min;
+}
+
 function userInfo() {
 
     // info stored as: User1#### {firstName, surName, email, joinDate, password, friends, followers}
-    let user  = JSON.parse(localStorage.getItem("User10000"));
+    let user  = JSON.parse(localStorage.getItem("user10000"));
     let joinDate = new Date(user.joinDate);
 
     const monthNames = ["January", "February", "March",     "April",   "May",      "June",
@@ -31,29 +42,97 @@ function addPost() {
         nextPostNum = localStorage.getItem("nextPostNum");
     } else {
         nextPostNum = 10000;
-        localStorage.setItem("nextPostNum", nextPostNum);
+        localStorage.setItem("nextPostNum", nextPostNum++);
     }
+
+    let user  = JSON.parse(localStorage.getItem("user10000"));
+
+    let today = getTimeStamp();
 
     let post = document.getElementById("post-field").value;
     console.log(`This is another test: ${post}`);
 
-    let newPost = document.createElement("div");
-    newPost.id = `post${nextPostNum}`;
-    newPost.className = "post";
-    newPost.innerHTML = post;
-
-    document.getElementById("feed-posts").appendChild(newPost);
-
     nextPostNum++;
     localStorage.setItem("nextPostNum", nextPostNum);
-    document.getElementById("post-field").value = "";
+    localStorage.setItem(`post${nextPostNum}`, JSON.stringify({"author": user.firstName+" "+user.surName, "timestamp": today, "post": post}));
+
+    document.querySelector("#post-field").value = "";
+
+    displayPosts();
 
 }
 
-function addComment() {
-    
+function addComment(n) {
+    let postNum = n;
+    console.log(`Will This Work?? (${postNum})` );
+
+    let nextCommentNum;
+    console.log("This is another test");
+    if (localStorage.getItem("nextCommentNum") !== null) {
+        nextCommentNum = localStorage.getItem("nextCommentNum");
+    } else {
+        nextCommentNum = 10000;
+        localStorage.setItem("nextPostNum", nextCommentNum++);
+    }
+
+    let user  = JSON.parse(localStorage.getItem("user10000"));
+    let today = getTimeStamp();
+
+    let comment = document.getElementById(`comment-field${postNum}`).value;
+
+    nextCommentNum++;
+    localStorage.setItem("nextCommentNum", nextCommentNum);
+
+    localStorage.setItem(`comment${postNum}${nextCommentNum}`, JSON.stringify({"author": user.firstName+" "+user.surName, "timestamp": today, "comment": comment}));
+    document.getElementById(`comment-field${postNum}`).value = "";
+
+    displayPosts();
+
+}
+
+function displayPosts() {
+
+    document.getElementById("feed-posts").innerHTML = "";
+
+    for (let num = 11000; num > 10000; num--) {
+        if (localStorage.getItem(`post${num}`) !== null) {
+            // posts are store as post1####: author, timestamp, post
+            let post = JSON.parse(localStorage.getItem(`post${num}`));
+
+            let newPost = document.createElement("div");
+            newPost.id = `post${num}`;
+            newPost.className = "post";
+            newPost.innerHTML += `<div class="user-info"><img src="images/avatar.png" height="25" />&nbsp;${post.author} (${post.timestamp})</div>`;
+            newPost.innerHTML += `<div class="post-info">"${post.post}"</div>`;
+            newPost.innerHTML += `<div class="sub-comments"><input id="comment-field${num}" class="comment-field" type="text" value="" placeholder="What Ya Got To Say About That??" /><input id="submit-comment${num}" class="comment-button" type="submit" value="Comment" /></div>`;
+            newPost.innerHTML += `<div id="comments${num}"></div>`;
+        
+            document.getElementById("feed-posts").appendChild(newPost);
+        
+            document.querySelector(`#submit-comment${num}`).addEventListener("click", function() {
+                addComment(num);
+            });
+
+            for (let nextNum = 10000; nextNum < 11000; nextNum++) {
+                if (localStorage.getItem(`comment${num}${nextNum}`) !== null) {
+                    let comment = JSON.parse(localStorage.getItem(`comment${num}${nextNum}`));
+
+                    let newComment = document.createElement("div");
+                    newComment.id = `comment${num}${nextNum}`;
+                    newComment.className = "comment-box";
+                    newComment.innerHTML = `<img src="images/avatar.png" height="15" />`;
+                    newComment.innerHTML += `&nbsp;${comment.author} (${comment.timestamp})`;
+                    newComment.innerHTML += `<br />"${comment.comment}"`;
+                
+                    document.getElementById(`comments${num}`).appendChild(newComment);
+                
+                }
+            }
+        }    
+    }
 }
 
 window.addEventListener("load", userInfo);
+window.addEventListener("load", displayPosts);
 
 document.querySelector("#submit-post").addEventListener("click", addPost);
--- sign_up.js
+++ sign_up.js
@@ -1,21 +1,89 @@
 function main(){
-    let first_name = document.getElementById('first_n').value;
-    let last_name = document.getElementById('last_n').value;
-    let birthday = document.getElementById('birth').value;
-    let email = document.getElementById('email').value.indexOf("@");
-    let password = document.getElementById('password');
-    let confirm = document.getElementById('confirm_pass');
-    
-    let err = document.getElementById('error');
+    let first_name = document.getElementById("first_n").value;
+    console.log(first_name);
 
-    if (email == -1) {
-        alert("Not a valid e-mail!");
-        submitOK = "false";
+    let last_name = document.getElementById("last_n").value;
+    let birthday = document.getElementById("birth").value;
+    let timecheck = Date.parse(birthday);
+    let email = document.getElementById("email").value.indexOf("@");
+    let password = document.getElementById("password");
+    let confirm = document.getElementById("confirm_pass");
+    
+    let emailOK = false;
+    let passOK = false;
+    let firstOK = false;
+    let lastOK = false;
+    let dobOK = false;
+
+    console.log(last_name);
+    console.log(birthday);
+    console.log(email);
+    console.log(password);
+
+    let first_err = document.getElementById("first_err");
+    let last_err = document.getElementById("last_err");
+    let dob_err = document.getElementById("dob_err");
+
+    // Check if first name field is filled out
+    if (first_name.length < 1) {
+        document.getElementById("first_err").innerHTML = "* Please Enter a First Name"
+      } else {
+          firstOK = true;
       }
-    
 
-    if(password.value != confirm.value){
-        err.innerHTML = "Passwords do not match";
-        // throw "Passwords do not match"
+    // Check if last name field is filled out
+    if (last_name.length < 1) {
+        document.getElementById("last_err").innerHTML = "* Please Enter a Last Name"
+      } else {
+          firstOK = true;
+      }
+
+    // Checks if there is a @ in the email field
+    if (email == -1) {
+        document.getElementById("email_err").innerHTML = "* Invalid Email Address"
+      } else {
+          emailOK = true;
+      }
+
+    // checks if the password and confirm are the same
+    if (password.value != confirm.value){
+        document.getElementById("pass_err").innerHTML = "* Passwords do not match";    
+    } else {
+        passOK = true;
     }
+
+    // checks if a valid date was entered
+    if (isNaN(timecheck)){
+        document.getElementById("dob_err").innerHTML = "* Invalid Date of Birth Entered";    
+    } else {
+        passOK = true;
+    }
+
+    if (emailOK && passOK && lastOK && firstOK && dobOK) {
+        let today = new Date(Date.now());
+
+        let nextUserNum;
+        console.log("This is a test");
+        if (localStorage.getItem("nextUserNum") !== null) {
+            nextUserNum = localStorage.getItem("nextPostNum");
+        } else {
+            nextUserNum = 10000;
+            localStorage.setItem("nextUserNum", nextUserNum++);
+        }
+
+        localStorage.setItem(`User${nextUserNum}`, JSON.stringify({
+                "firstName":first_name, "lastName": last_name, "email": email, 
+                "joinDate": today, "password": password}));
+
+        sessionStorage.setItem("userName", `${first_name} ${last_name}`);
+        sessionStorage.setItem("joinDate", today);
+    }   
 }
+
+// -----------This is an example of how to store & retrieve an object in the local storage -------------------------
+// localStorage.setItem('myObject', JSON.stringify({'prop1': "string", 'prop2': "string"}));
+// let temp = JSON.parse(localStorage.get('myObject'));
+// temp.prop1;
+// -----------------------------------------------------------------------------------------------------------------
+
+// info stored as: User1#### {firstName, surName, email, joinDate, password, friends, followers}
