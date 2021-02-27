f7669d987ea88f6638675d96f0c3e0f4e8db4fe0
oneilljp-keyin <john.oneill@keyin.com>
2021-02-27 16:35:18 -0330

    minor edit

--- mode.js
+++ mode.js
@@ -5,11 +5,9 @@ let currentTheme = localStorage.getItem("theme");
 
 function setMode() {
     if (currentTheme == "light") {
-        // css.href = "yesby-light.css";
         css.setAttribute("href", "yesby-light.css");
         btn.value = "Dark Mode";
     } else if (currentTheme == "dark") {
-        // css.href = "yesby-dark.css";
         css.setAttribute("href", "yesby-dark.css");
         btn.value = "Light Mode";
     }
@@ -20,12 +18,10 @@ function changeMode() {
     console.log(css);
 
     if (css.href.search("yesby-light") == -1) {
-        // css.href = "yesby-light.css";
         css.setAttribute("href", "yesby-light.css");
         localStorage.setItem("theme", "light");
         btn.value = "Dark Mode";
     } else if (css.href.search("yesby-dark") == -1) {
-        // css.href = "yesby-dark.css";
         css.setAttribute("href", "yesby-dark.css");
         localStorage.setItem("theme", "dark");
         btn.value = "Light Mode";
